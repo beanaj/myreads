c09865807cc5484155b8444c786f7b27f62d1653
jj790d <jj790d@att.com>
2018-12-11 01:07:28 -0500

    Made sure they display on correct shelf in search page

--- src/components/Book.js
+++ src/components/Book.js
@@ -16,10 +16,11 @@ class Book extends React.Component {
                     <div className="book-shelf-changer">
                         <select onChange={this.handleShelfChange} value={this.props.book.shelf}>
                             <option value="move" disabled>Move to...</option>
+                            <option value="none">None</option>
                             <option value="currentlyReading">Currently Reading</option>
                             <option value="wantToRead">Want to Read</option>
                             <option value="read">Read</option>
-                            <option value="none">None</option>
+
                         </select>
                     </div>
                 </div>
